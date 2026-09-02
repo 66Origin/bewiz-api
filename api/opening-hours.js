24ce08f98e1b0a6840c84ab93005c08cc7ce8452
christelle-bqn <christellebqndev@gmail.com>
2026-09-02 13:30:12 +0200

    fix: ensure Vary header is set for CORS response

--- api/opening-hours.js
+++ api/opening-hours.js
@@ -11,6 +11,7 @@ export default async function handler(req, res) {
     res.setHeader("Access-Control-Allow-Origin", origin);
   }
 
+  res.setHeader("Vary", "Origin");
   res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
